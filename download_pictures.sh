b4e7f54ee89f45924e81501bb0bed314dfe45226
Ignatz <iggi@m3lt.work>
2025-10-31 06:32:40 +0100

    start working on downloader script

---
+++ download_pictures.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p bash -p xmlstarlet -p jq -p curl
+
+file=$1
+
+function build_cmd(){
+  xmlstarlet sel -t -m "${1}" -v "${2}" -n "${file}"
+}
+
+base_query='/cockatrice_deck/zone[@name="main"]/card'
+
+function images_by_uuid(){
+  build_cmd "${base_query}[@uuid]" "@uuid" | while read -r uuid
+  do
+    curl -s "https://api.scryfall.com/cards/${uuid}" 
+  done
+}
+
+function images_by_name(){
+  build_cmd "${base_query}[not(@uuid)]" "@name" | while read -r name
+  do
+    curl --get -s --data-urlencode "exact=${name}" "https://api.scryfall.com/cards/named"
+  done
+}
+
+function extract_img_url() {
+  jq -r '.image_uris.large'
+}
+
+function download_img() {
+  while read -r url
+  do
+    wget -q "${url}"
+  done
+}
+
+
+(images_by_name ; images_by_uuid) | extract_img_url | download_img
